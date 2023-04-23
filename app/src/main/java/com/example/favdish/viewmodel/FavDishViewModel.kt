79ac6b1fdfe851211736eed1e6559a6aecbc2028
billyyan <By869422>
2023-04-22 18:56:02 -0700

    Create Model Classes

---
+++ app/src/main/java/com/example/favdish/viewmodel/FavDishViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.favdish.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.favdish.model.database.FavDishRepository
+import com.example.favdish.model.entities.FavDish
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class FavDishViewModel(private val repository: FavDishRepository) : ViewModel() {
+
+    fun insert(dish: FavDish) = viewModelScope.launch {
+        repository.insertFavDishData(dish)
+    }
